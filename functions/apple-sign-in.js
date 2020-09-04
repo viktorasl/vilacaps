eebfacc96a3df2f0f6812ca8d08e46d12ee36cd3
Viktoras Laukevičius <viktoras.laukevicius@yahoo.com>
2020-09-04 11:21:40 +0300

    fixup! Apple sign in aws function

--- functions/apple-sign-in.js
+++ functions/apple-sign-in.js
@@ -6,12 +6,29 @@ exports.handler = async (event, context, callback) => {
     //     "key": "value2"
     //   })
     // };
-    const response = {
-      statusCode: 301,
-      headers: {
-        Location: 'https://appleid.apple.com/auth/authorize?client_id=com.fitpassu.fitpassu.beta.azureb2c&redirect_uri=https://vilacaps.com/.netlify/functions/azure-b2c-sign-in-with-apple-proxy&response_type=code&scope=email&response_mode=form_post&ui_locales=en',
+    // const response = {
+    //   statusCode: 301,
+    //   headers: {
+    //     Location: 'https://appleid.apple.com/auth/authorize?client_id=com.fitpassu.fitpassu.beta.azureb2c&redirect_uri=https://vilacaps.com/.netlify/functions/azure-b2c-sign-in-with-apple-proxy&response_type=code&scope=email&response_mode=form_post&ui_locales=en',
+    //   }
+    // };
+    let response
+    try {
+      response = await fetch('https://appleid.apple.com/auth/authorize?client_id=com.fitpassu.fitpassu.beta.azureb2c&redirect_uri=https://vilacaps.com/.netlify/functions/azure-b2c-sign-in-with-apple-proxy&response_type=code&scope=email&response_mode=form_post&ui_locales=en')
+      // handle response
+    } catch (err) {
+      return {
+        statusCode: err.statusCode || 500,
+        body: JSON.stringify({
+          error: err.message
+        })
       }
-    };
-  
-    return callback(null, response);
+    }
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        data: response
+      })
+    }
+    // return callback(null, response);
 };
