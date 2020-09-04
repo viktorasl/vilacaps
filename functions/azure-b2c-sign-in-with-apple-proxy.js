739e7a8ec5ce0967dafbae0616ba1a66680ec01d
Viktoras Laukevičius <viktoras.laukevicius@yahoo.com>
2020-09-04 17:09:38 +0300

    Successfuly finish auth

--- functions/azure-b2c-sign-in-with-apple-proxy.js
+++ functions/azure-b2c-sign-in-with-apple-proxy.js
@@ -1,11 +1,13 @@
 exports.handler = async (event, context, callback) => {
   console.log(JSON.stringify(event));
   console.log(JSON.stringify(context));
+
+  // https://vilacaps.com/sign-in-with-apple.html?client_id=com.fitpassu.fitpassu.beta.azureb2c&redirect_uri=https%3a%2f%2ffitpassuprod.b2clogin.com%2ffitpassuprod.onmicrosoft.com%2foauth2%2fauthresp&response_type=id_token&scope=email+name&response_mode=form_post&nonce=lnnpyGfESK%2bJt4BEDNUJLg%3d%3d&ui_locales=en&state=StateProperties%3deyJTSUQiOiJ4LW1zLWNwaW0tcmM6NTc0N2MyMGEtZDQ0Ni00OTY1LWFkMjMtODZiMWNjMTRlNjJlIiwiVElEIjoiYzQ1Y2EyODgtNGVjMi00YjhkLThhZDUtZDIyNDU4MTk4MjEwIiwiVE9JRCI6IjEzYmVjZWRhLTg4ODgtNGI4OS04NzYxLWNiZWQyNGRhMmE4MyJ9
+
   return {
-    statusCode: 404,
+    statusCode: 200,
     body: JSON.stringify({
-      "event": event,
-      "context": context
+      "event": event
     })
   };
   // const response = {
