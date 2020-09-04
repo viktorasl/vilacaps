ffb2621fb4061d8ae6caaa8f48f64253f141c4e6
Viktoras Laukevičius <viktoras.laukevicius@yahoo.com>
2020-09-04 09:52:56 +0300

    Log aws function event

--- functions/azure-b2c-sign-in-with-apple-proxy.js
+++ functions/azure-b2c-sign-in-with-apple-proxy.js
@@ -1,16 +1,17 @@
 exports.handler = async (event, context, callback) => {
-  // return {
-  //   statusCode: 200,
-  //   body: JSON.stringify({
-  //     "key": "value"
-  //   })
-  // };
-  const response = {
-    statusCode: 301,
-    headers: {
-      Location: 'https://fitpassuprod.b2clogin.com/fitpassuprod.onmicrosoft.com/oauth2/authresp',
-    }
+  console.log(JSON.stringify(event));
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      "key": "value2"
+    })
   };
+  // const response = {
+  //   statusCode: 301,
+  //   headers: {
+  //     Location: 'https://fitpassuprod.b2clogin.com/fitpassuprod.onmicrosoft.com/oauth2/authresp',
+  //   }
+  // };
 
-  return callback(null, response);
+  // return callback(null, response);
 };
