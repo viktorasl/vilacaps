c9f14ac5ff7c42e45a29eccad6333ab1765fe27d
Viktoras Laukevičius <viktoras.laukevicius@yahoo.com>
2020-09-04 09:47:59 +0300

    Redirect to fitpassu azure b2c

--- functions/azure-b2c-sign-in-with-apple-proxy.js
+++ functions/azure-b2c-sign-in-with-apple-proxy.js
@@ -1,8 +1,16 @@
-exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      "key": "value"
-    })
+exports.handler = async (event, context, callback) => {
+  // return {
+  //   statusCode: 200,
+  //   body: JSON.stringify({
+  //     "key": "value"
+  //   })
+  // };
+  const response = {
+    statusCode: 301,
+    headers: {
+      Location: 'https://fitpassuprod.b2clogin.com/fitpassuprod.onmicrosoft.com/oauth2/authresp',
+    }
   };
+
+  return callback(null, response);
 };
