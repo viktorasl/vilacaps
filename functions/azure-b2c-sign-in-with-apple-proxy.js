3744d841b1fda58bd5bafb247546e3ae52e0fc22
Viktoras Laukevičius <viktoras.laukevicius@yahoo.com>
2020-09-05 16:24:41 +0300

    Intercept auth callback

--- functions/azure-b2c-sign-in-with-apple-proxy.js
+++ functions/azure-b2c-sign-in-with-apple-proxy.js
@@ -2,20 +2,23 @@ const fetch = require('node-fetch');
 
 exports.handler = async (event, context, callback) => {
   console.log(JSON.stringify(event));
-
-  return await fetch('https://appleid.apple.com/auth/token', {
-    method: 'post',
-		body: event['body'],
-    headers: {'Content-Type': 'application/x-www-form-urlencoded'} })
-    .then(response => response.json())
-    .then(data => {
-      console.log(JSON.stringify(data))
-      return {
-        statusCode: 200,
-        body: JSON.stringify(data)
-      }
-    })
-    .catch(error => ({ statusCode: 422, body: String(error) }));
+  return {
+    statusCode: 200,
+    body: JSON.stringify({})
+  }
+  // return await fetch('https://appleid.apple.com/auth/token', {
+  //   method: 'post',
+	// 	body: event['body'],
+  //   headers: {'Content-Type': 'application/x-www-form-urlencoded'} })
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     console.log(JSON.stringify(data))
+  //     return {
+  //       statusCode: 200,
+  //       body: JSON.stringify(data)
+  //     }
+  //   })
+  //   .catch(error => ({ statusCode: 422, body: String(error) }));
   // client_id=com.fitpassu.fitpassu.beta.azureb2c&
   // client_secret=eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjVXM0s5UjVTODQifQ.eyJhdWQiOiJodHRwczovL2FwcGxlaWQuYXBwbGUuY29tIiwic3ViIjoiY29tLmZpdHBhc3N1LmZpdHBhc3N1LmJldGEuYXp1cmViMmMiLCJpc3MiOiI2QjM0QVFDWTU1IiwiZXhwIjoxNjAxOTAwMTU3LCJpYXQiOjE1OTkzMDAxNTd9.3Lbh62R3nXbhjDpVmFFzyrd0S2f2NWL9_sp3Hwzxtyw2tZldGjhaGCqhF0nvUYAbeavBO1kh1TLUhWN7Z7hJ7w&
   // redirect_uri=https%3a%2f%2ffitpassuprod.b2clogin.com%2ffitpassuprod.onmicrosoft.com%2foauth2%2fauthresp&
