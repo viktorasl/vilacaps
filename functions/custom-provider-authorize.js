6d09c64391f977a6322d538328f3a35769dadd0f
Viktoras Laukevičius <viktoras.laukevicius@yahoo.com>
2020-09-07 14:02:51 +0300

    Check if calls token endpoint

--- functions/custom-provider-authorize.js
+++ functions/custom-provider-authorize.js
@@ -1,15 +1,15 @@
 exports.handler = async (event, context, callback) => {
     console.log(JSON.stringify(event));
-    const redirect_uri = event["queryStringParameters"]["redirect_uri"]
-    const state = event["queryStringParameters"]["state"]
-    const code = "AQDZRaj8eVLGJwULb7vyXPCLk8CBThisIsCode"
-    const loc = redirect_uri + "?" + "code=" + code + "&state=" + state
-    console.log(loc);
-    const response = {
-        statusCode: 301,
-        headers: {
-          Location: loc
-        }
-    };
-    return callback(null, response);
+    // const redirect_uri = event["queryStringParameters"]["redirect_uri"]
+    // const state = event["queryStringParameters"]["state"]
+    // const code = "AQDZRaj8eVLGJwULb7vyXPCLk8CBThisIsCode"
+    // const loc = redirect_uri + "?" + "code=" + code + "&state=" + state
+    // console.log(loc);
+    // const response = {
+    //     statusCode: 301,
+    //     headers: {
+    //       Location: loc
+    //     }
+    // };
+    // return callback(null, response);
 }
