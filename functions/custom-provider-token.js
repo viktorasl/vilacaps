a7d6cc53aa13b62174d76f9bfb6e3320869e0219
Viktoras Laukevičius <viktoras.laukevicius@yahoo.com>
2020-09-07 14:41:42 +0300

    Return empty

--- functions/custom-provider-token.js
+++ functions/custom-provider-token.js
@@ -20,7 +20,7 @@ exports.handler = async (event, context, callback) => {
             console.log(JSON.stringify(data))
             return {
                 statusCode: 200,
-                body: JSON.stringify(data)
+                body: JSON.stringify({})
             }
         })
         .catch(error => {
